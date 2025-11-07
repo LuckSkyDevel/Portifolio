3086963197b4f0f2039a990d39112c001e987f6f
LUCAS DE OLIVEIRA CARVALHO <lucas.ocarvalho@cbm.df.gov.br>
2025-11-07 14:01:43 -0300

    Insert Download of CV!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import techGif from '../assets/tech2.gif';
 import { motion } from 'motion/react';
+import { saveAs } from 'file-saver';
+import { BsArrowRightCircleFill } from "react-icons/bs";
 
 export default function Home() {
     const transition = {
@@ -11,13 +13,15 @@ export default function Home() {
         ease: [0, 0.71, 0.2, 1.01],
     };
 
+    const downloadCV = () => { saveAs("https://drive.google.com/file/d/10PMTWQ1EmX1Ey4-ACS8hogiY3x6jnaUF", "CVLucasCarvalho.pdf")};
+
     return (
         <>
-            <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-between w-full h-80 lg:h-150 px-5 lg:px-20 py-0">
+            <div id="home" className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-between w-full h-150 lg:h-150 px-5 lg:px-20 py-0">
                 <div className="absolute size-100 lg:size-250 right-0 lg:right-20 z-10">
                     <img src={techGif} alt="" />
                 </div>
-                <div className="relative flex-1 flex-col items-start justify-center top-10 z-20">
+                <div className="relative flex flex-1 flex-col items-center lg:items-start justify-center text-center lg:text-left top-10 z-20">
                     <motion.h1 className="primary-heading" initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={transition}>
                         Welcome To My Personal Portifolio
                     </motion.h1>
@@ -44,9 +48,9 @@ export default function Home() {
                             <FaLinkedin />
                         </motion.a>
                     </div>
-                    {/* <button className="secondary-button">
-            Download CV <FiArrowRight />{" "}
-          </button> */}
+                    <button className="secondary-button" onClick={downloadCV}>
+                        Download CV <BsArrowRightCircleFill />
+                    </button>
                 </div>
             </div>
         </>
